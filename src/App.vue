d2686ed45348ddaace98f1f95a060b81b187df5a
RafaAlbano <rafaelaalbano70@gmail.com>
2022-04-29 11:34:52 -0300

    projeto

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import BlocoPrincipal from "@/components/BlocoPrincipal.vue";
+import BlocoPrincipal from "@/components/BlocoCabecalho.vue";
+import BlocoPrincipal from "@/components/BlocoRodape.vue";
+
+
+export default {
+  components: { BlocoPrincipal, BlocoCabecalho, BlocoRodape },
+};
+</script>
+<template>
+  <BlocoPrincipal />
+  <BlocoCabecalho />
+  <BlocoRodape />
+</template>
+
+<style></style>
---
+++ src/components/BlocoPrincipal.vue
@@ -0,0 +1,5 @@
+<script></script>
+<template>
+    <main id="pag">Teste Vocaciolal</main>
+</template>
+ <style></style>
