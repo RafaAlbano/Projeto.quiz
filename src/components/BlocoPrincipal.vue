0831c49f568ee6e64254a4e0ae2a934834ec5edf
RafaAlbano <rafaelaalbano70@gmail.com>
2022-05-04 15:04:17 -0300

    040522

--- src/components/BlocoPrincipal.vue
+++ src/components/BlocoPrincipal.vue
@@ -1,21 +1,12 @@
-<script></script>
+<script>
+import BlocoQuestoes from "@/components/BlocoQuestoes.vue";
+export default {
+  components: { BlocoQuestoes },
+};
+</script>
 <template>
   <main id="pag">
-    <h1>Teste Vocacional</h1>
-    <div id="esquerda">
-      <h3>1 Na escola, quais eram suas matérias preferidas?</h3>
-    </div>
-    <div id="direita">
-      <p>
-        a) Matemática e física. Sempre que podia, ajudava meus colegas a
-        descobrir a quantos quilômetros por hora anda um batmóvel que sai de
-        Gotham City e chega a Lajeado em menos de trinta minutos. <br />
-        b) Português, inglês, filosofia e sociologia. Já perdi as contas de
-        quantas fanfics sobre o Shawn Mendes escrevi nas aulas de redação…<br />c)
-        Química e biologia. Estudar os fenômenos da natureza e do corpo humano
-        foi minha religião durante o ensino médio inteirinho. <br />
-      </p>
-    </div>
+    <BlocoQuestoes />
   </main>
 </template>
 <style></style>
---
+++ src/components/BlocoQuestoes.vue
@@ -0,0 +1,73 @@
+<script>
+export default {
+  data() {
+    return {
+      pergunta: "1 Na escola, quais eram suas matérias preferidas?",
+      opcoes: [
+        {
+          id: 1,
+          opcao: "Matemática e física.",
+          correta: false,
+        },
+        {
+          id: 2,
+          opcao: "Português, inglês, filosofia e sociologia. ",
+          correta: false,
+        },
+        {
+          id: 3,
+          opcao: "Química e biologia. ",
+          correta: true,
+        },
+      ],
+    };
+  },
+};
+</script>
+<template>
+  <div class="questoes">
+    <div class="pergunta">{{ pergunta }}</div>
+    <div class="alternativas">
+      <p v-for="opcao of opcoes" :key="opcao.id">{{ opcao.opcao }}</p>
+    </div>
+  </div>
+</template>
+<style>
+.questoes {
+  display: flex;
+  height: 100%;
+}
+.pergunta {
+  width: 50%;
+  height: 100vh;
+  background-color: #c0aecd;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2.5em;
+  font-family: "Times New Roman", Times, serif;
+}
+
+.alternativas {
+  width: 50%;
+  height: 100vh;
+  background-color: #b19fbe;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  font-size: 2em;
+}
+
+.alternativas p {
+  width: 80%;
+  padding: 10px;
+}
+
+.alternativas p:hover {
+  background-color: #c0aecd;
+  cursor: pointer;
+}
+</style>
