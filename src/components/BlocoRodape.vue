807a132b6696a3acdd3dd6e26ac751a596980417
RafaAlbano <rafaelaalbano70@gmail.com>
2022-05-06 11:19:29 -0300

    060522

--- src/components/BlocoRodape.vue
+++ src/components/BlocoRodape.vue
@@ -1,5 +1,17 @@
 <script></script>
 <template>
-  <footer></footer>
+  <footer>
+    <p>Rafaela Albano e Heloisa Bett, 2info3</p>
+  </footer>
 </template>
-<style></style>
+<style>
+footer{
+  background-color: #C4C4C4 ;
+  font-family: 'Times New Roman', Times, serif;
+  text-align: right;
+  padding-right: 20px;
+  font-size: 1.3em;
+  padding-top: 3px;
+  padding-bottom: 0.3px;
+}
+</style>
